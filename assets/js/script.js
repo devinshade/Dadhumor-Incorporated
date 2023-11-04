2f73904314d8f8661550f85eca57072ee5b2869a
devinshade <devindanielle93@gmail.com>
2023-11-03 20:23:54 -0600

    added hover text to dad-joke card

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,3 +167,22 @@ function searchCity (cityName) {
     }
     
     getDadJoke();
+
+    // window.onload is optional since it depends on the way in which you fire events
+window.onload=function(){
+
+    // selecting the elements for which we want to add a tooltip
+    const target = document.getElementById("flashCardContainer");
+    const tooltip = document.getElementById("tooltip-text");
+    
+    // change display to 'block' on mouseover
+    target.addEventListener('mouseover', () => {
+      tooltip.style.display = 'block';
+    }, false);
+    
+    // change display to 'none' on mouseleave
+    target.addEventListener('mouseleave', () => {
+      tooltip.style.display = 'none';
+    }, false);
+    
+    }
